2201ded4d944e846f2fa5b39e9043cc113380963
Dondre <74502839+Drw2366@users.noreply.github.com>
2021-04-11 00:23:19 -0400

    Lab1 01b Getting up and running

---
+++ index.js
@@ -0,0 +1,22 @@
+    let name2 = window.prompt("What is your favorite movie?");
+    let response1 = 'Your favorite movie is: '; 
+    // console.log(response1 + name2);
+    alert(response1 + name2);
+        
+    let movie = prompt("Who is your favorite actor in that movie?");  
+    let actorMovie = 'Your favorite actor is: '; 
+    // console.log(actorMovie + movie); 
+    alert(actorMovie + movie);
+
+    let budget = prompt("How much money did they spend to make the movie?");
+    let money = 'Thats a lot of moolah lol: ';
+    // console.log(money + budget);
+    alert(money + budget);
+
+    let movie2 = prompt("Did the movie win an Acamedy Award?");
+    let win = 'Wow thats crazy!!!!! ';
+    // console.log(win + movie2);
+    alert(win + movie2);
+
+  
+    alert('Your favorite movie is ' + name2 + ' which they had a budget of '+ budget +', but they never won an Acamedy Award !!');
